e392481700e89779f62d76065b79dcec6b849156
caitlynstoked <119529327+caitlynstoked@users.noreply.github.com>
2025-09-12 09:33:30 -0600

    finalHWSep12

--- BikeShare.R
+++ BikeShare.R
@@ -66,6 +66,39 @@ dataset <- dataset %>%
     
 #heres my grid
 (p1 | p2) / (p3 | p4)
+    
+    
+my_linear_model <- linear_reg() |> 
+  set_engine('lm') |> 
+  set_mode('regression') |> 
+  fit(formula=Response)
+
+trainData<-vroom("Bike Data/train.csv") 
+trainData<- trainData |> 
+  select(-casual, -registered)
+testData<-vroom("Bike Data/test.csv")
+library(tidymodels)
+## Setup and Fit the Linear Regression Model
+my_linear_model <- linear_reg() %>%  #Type of model
+  set_engine("lm") %>% # Engine = What R function to use
+  set_mode("regression") %>% # Regression just means quantitative response
+  fit(formula= count~.-datetime, data=trainData)## Generate Predictions Using Linear Model
+
+  bike_predictions <- predict(my_linear_model, 
+                              new_data=testData) # Use fit to predict
+  bike_predictions 
+  
+## Format the Predictions for Submission to Kaggle
+  kaggle_submission <- bike_predictions %>%
+    bind_cols(., testData) %>% #Bind predictions with test data
+    select(datetime, .pred) %>% #Just keep datetime and prediction variables
+    rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
+    mutate(count=pmax(0, count)) %>% #pointwise max of (0, prediction)
+    mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
+    
+  ## Write out the file
+    vroom_write(x=kaggle_submission, file="./LinearPreds.csv", delim=",")
+    
 ##### I spent a while messing with the data here only to find something ######
 #####       Thats already described in the data description             ###########
 
